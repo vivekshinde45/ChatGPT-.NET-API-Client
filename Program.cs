3c48679c42fa6aaf2aac9db734473ac4ba7168c5
Vivek Shinde <85720426+vivekshinde45@users.noreply.github.com>
2023-03-15 13:23:43 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+if (args.Length > 0)
+{
+    var postAPIurl = "https://api.openai.com/v1/completions";
+    var openaiAPIKey = "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";
+    HttpClient client = new HttpClient();
+
+    client.DefaultRequestHeaders.Add("authorization", $"Bearer {openaiAPIKey}");
+    var content = new StringContent("{\"model\": \"text-davinci-001\", \"prompt\": \"" + args[0] + "\", \"temperature\": 1, \"max_tokens\": 100}",
+        Encoding.UTF8, "application/json");
+
+    HttpResponseMessage response = await client.PostAsync(postAPIurl, content);
+    var responseString = await response.Content.ReadAsStringAsync();
+
+    try
+    {
+        var dynamicData = JsonConvert.DeserializeObject<dynamic>(responseString);
+
+        var guess = GuessCommand(dynamicData!.choices[0].text);
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"--> My guess at the command prompt is: {guess}");
+        Console.ResetColor();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"--> cannot able to deseriealize {ex.Message}");
+    }
+
+    Console.WriteLine(responseString);
+}
+else
+{
+    Console.WriteLine("---> You need to provide some input");
+}
+
+
+static string GuessCommand(string raw)
+{
+    Console.WriteLine("--> GPT-3 API Returned Text:");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine(raw);
+
+    var lastIndex = raw.LastIndexOf("\n");
+
+    var guess = raw.Substring(lastIndex + 1);
+    Console.ResetColor();
+
+    // copy guess to clipboard
+    TextCopy.ClipboardService.SetText(guess);
+
+    return guess;
+}
